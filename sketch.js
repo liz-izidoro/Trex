395f2d0a15ba2652c92ab8cf969b1bbf336eae80
andressalessa <lessadasilveiraandressa@gmail.com>
2022-03-24 20:01:14 -0300

    primeiro commit jogo trex

---
+++ sketch.js
@@ -0,0 +1,35 @@
+// Variável Global
+var trex ,trex_running;
+var edges;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crie um sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+
+  edges = createEdgeSprites();
+  console.log(edges);
+}
+
+function draw(){
+  background("white");
+
+  if (keyDown('space')) {
+    trex.velocityY = -10;
+  }
+
+  trex.velocityY = trex.velocityY + 0.5;
+  trex.collide(edges[3]);
+
+
+  drawSprites();
+
+}
