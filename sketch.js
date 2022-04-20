19693ed0d77825cff327dc1874f2c9f1b39a9ef8
liz-izidoro <102263665+liz-izidoro@users.noreply.github.com>
2022-04-20 19:24:32 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,11 +3,18 @@ var trex ,trex_running;
 var edges;
 var ground, groundImg;
 var cloud, cloudImg;
+var cacto1, cacto2, cacto3, cacto4, cacto5, cacto6;
 
 function preload(){
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   groundImg = loadImage("ground2.png");
   cloudImg = loadImage("cloud.png");
+  cacto1 = loadImage("obstacle1.png");
+  cacto2 = loadImage("obstacle2.png");
+  cacto3 = loadImage("obstacle3.png");
+  cacto4 = loadImage("obstacle4.png");
+  cacto5 = loadImage("obstacle5.png");
+  cacto6 = loadImage("obstacle6.png");
 }
 
 function setup(){
@@ -34,7 +41,6 @@ function draw(){
   
   if (keyDown('space') && trex.y >= 100) {
     trex.velocityY = -10;
-  
   }
 
   // Resolve o problema do ch�o sumir
@@ -48,7 +54,7 @@ function draw(){
   trex.collide(ground);
 
   createClouds();
-
+  createCactos();
   drawSprites();
 }
 
@@ -60,6 +66,43 @@ function createClouds() {
     cloud.y = Math.round(random(10,  60));
     cloud.scale = 0.5;
     cloud.velocityX = -2;
+    cloud.lifetime = 300;
+  }
+}
+function createCactos() {
+var cacto;
+  if (frameCount % 60 === 0) {
+    cacto = createSprite(600, 160, 40, 10);
+    cacto.velocityX = -6;
+var aleatorio = Math.round(random(1, 6));
+    switch (aleatorio) {
+      case 1:
+        cacto.addImage(cacto1);
+        break;
+      case 2:
+        cacto.addImage(cacto2);
+        break;
+      case 3:
+        cacto.addImage(cacto3);
+        break;
+      case 4:
+        cacto.addImage(cacto4);
+        break;
+      case 5:
+        cacto.addImage(cacto5);
+        break;
+      case 6:
+        cacto.addImage(cacto6);
+        break;
+    
+      default:
+        break;
+    }
+    cacto.scale = 0.5;
+    cacto.lifetime = 150;
+    // cloud.y = Math.round(random(10,  60));
+    // cloud.scale = 0.5;
+    // cloud.velocityX = -2;
   }
   
 }
